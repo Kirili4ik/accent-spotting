5ec02e5c846fdcad88e6c87e9a6b0830bafb57f8
arinaruck <arina.ruck@gmail.com>
2020-12-17 05:49:04 +0300

    model update

--- model.py
+++ model.py
@@ -8,6 +8,16 @@ from torch.utils.data import Dataset, DataLoader, random_split
 import torchvision.transforms as transforms
 import numpy as np
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchaudio
+import os
+from torch.utils.data import Dataset, DataLoader, random_split
+import torchvision.transforms as transforms
+import numpy as np
+
 class TSC(nn.Module):
     def __init__(self, kernel_size, in_channels, out_channels, n_groups=1, 
                  dilation=1):
@@ -92,47 +102,19 @@ class QuarzNet(nn.Module):
         super().__init__() 
         self.config = config
         self.net = nn.Sequential(
-            TSCActivated(**config['c1']),
-            TSCBlock(**config['b1']),
-            TSCBlock(**config['b2']),
-            TSCBlock(**config['b3']),
-            TSCBlock(**config['b4']),
-            TSCBlock(**config['b5']),
-            TSCActivated(**config['c2']),
-            Debug(),
-            TSCActivated(**config['c3']),
-            Debug()
+            TSCActivated(*config['c1']),
+            TSCBlock(*config['b1']),
+            TSCBlock(*config['b2']),
+            TSCBlock(*config['b3']),
+            TSCBlock(*config['b4']),
+            TSCBlock(*config['b5']),
+            TSCActivated(*config['c2']),
+            TSCActivated(*config['c3']),
         )
 
     def forward(self, x):
         x = self.net(x)
         return x
-
-
-class ClassificationNet(nn.Module):
-    
-    def __init__(self, config):
-        super().__init__() 
-        self.encoder = QuarzNet(config)
-        hidden_size, attn_size = config['hidden_size'], config['attn_size']
-        n_classes = config['n_classes']
-        self.attention_scores = nn.Sequential(
-            nn.Linear(hidden_size, attn_size),
-            nn.Tanh(),
-            nn.Linear(attn_size, 1, bias=False),
-            nn.Softmax(dim=1)
-        )
-        self.out = nn.Linear(hidden_size, n_classes)
-        
-        
-    def forward(self, x):
-        x = x.unsqueeze(1)
-        x = self.encoder(x)
-        x = x.transpose(1, 2)
-        attn_weights = self.attn(x)
-        x = torch.bmm(attn_weights.transpose(1, 2), x)
-        out = self.out(x)
-        return out.squeeze(1), attention_weights
 
 
 def make_param_dict(names, params):
@@ -142,26 +124,50 @@ def make_param_dict(names, params):
 
 def make_config():
     n_mels = 80
-    n_classes = 34
+    n_classes = 35
     config = {
         #  k, in, out, dilation
-        'c1': [33, n_mels, 128, 1],
+        'c1': [33, n_mels, 256, 1],
         'c2': [87, 512, 512, 2],
-        'c3': [1, 512, 512, 1],
+        'c3': [1, 512, 1024, 1],
         # n_blocks, k, in, out
-        'b1': [5, 33, 128, 128],
-        'b2': [5, 39, 128, 256],
-        'b3': [5, 51, 256, 256],
-        'b4': [5, 63, 256, 512],
+        'b1': [5, 33, 256, 256],
+        'b2': [5, 39, 256, 256],
+        'b3': [5, 51, 256, 512],
+        'b4': [5, 63, 512, 512],
         'b5': [5, 75, 512, 512],
-        'hidden_size': 512,
-        'attn_size': 256,
+        'hidden_size': 1024,
+        'attn_size': 512,
         'n_classes' : n_classes,
-        'n_epochs': 25,
+        'n_epochs': 25, 
         'n_mels': n_mels,
         'device': torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
+        
     }
 
     return config
 
+class ClassificationNet(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__() 
+        self.encoder = QuarzNet(config)
+        hidden_size, attn_size = config['hidden_size'], config['attn_size']
+        n_classes = config['n_classes']
+        self.attention_weights = nn.Sequential(
+            nn.Linear(hidden_size, attn_size),
+            nn.Tanh(),
+            nn.Linear(attn_size, 1, bias=False),
+            nn.Softmax(dim=1)
+        )
+        self.out = nn.Linear(hidden_size, n_classes)
+        
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = x.transpose(1, 2)
+        attn_weights = self.attention_weights(x)
+        x = torch.bmm(attn_weights.transpose(1, 2), x)
+        out = self.out(x)
+        return out.squeeze(1), attn_weights.squeeze(-1)
+        
